574b2ae6ea96ed97ed2702bfa4bfabff056dcb90
Aldo Miralles <alditomiralles3@gmail.com>
2024-04-19 00:36:01 -0600

    adding hostname for images

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "http",
         hostname: "localhost",
       },
+      {
+        protocol: "https",
+        hostname: "e-commerce-backend-nestjs-postgres.onrender.com",
+      },
     ],
   },
 };
