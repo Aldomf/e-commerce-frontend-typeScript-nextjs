f1edeb7dd8c6204c86fabf7d382f269690b08d08
gaspym97 <angelgasparmiralles@gmail.com>
2024-04-26 22:55:58 -0400

    adding button that changes the orders status

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -6,6 +6,7 @@ import { Order, OrderStatus, Status, } from "@/interfaces/interfaces";
 import { useMediaQuery } from "react-responsive";
 import HamburguerMenuAdmin from "@/components/admin/HamburguerMenuAdmin";
 import { useAuth } from "@/context/AuthContext";
+import { error } from "console";
 
 function AdminUser() {
   const { token } = useAuth();
@@ -91,6 +92,12 @@ function AdminUser() {
       selector: (row: Order) => row.orderStatus,
       sortable: true,
     },
+    {
+      name: "Change status",
+      cell: (row: Order) => <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => handleButtonClick(row.userId, row.orderStatus as Status)}>Press</button>
+    },
   ];
 
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 768px)" });
@@ -144,7 +151,43 @@ function AdminUser() {
     setPrevSearchQuery(searchQuery);
   };
 
-  const updateOrderStatus = async (id: number, status: OrderStatus) => {
+
+  //function that capitalize the string first letter
+  const capitalizeFirstLetter = (text: string): string => {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+  };
+
+  // array that store every order status
+  const statusOrder: Status[] = [
+    Status.Pending,
+    Status.Processing,
+    Status.Shipped,
+    Status.Cancelled,
+    Status.Delivered
+  ];
+
+  // function that changes to the next status 
+  const getNextStatus = (currentStatus: string): Status => {
+    // validate that currenStatus is a valid Status enum
+    if (!Object.values(Status).includes(currentStatus as Status)) {
+      console.log(currentStatus);
+      throw new Error('Invalid status');
+    }
+    // find actual index in the statusOrder array
+    const currentIndex = statusOrder.findIndex(status => status === currentStatus);
+
+    // if current index is -1, it means that the status isnt in the array
+    if (currentIndex === -1) {
+      throw new Error("Current status not found in status order")
+    }
+    // calculate next status index
+    const nextIndex = (currentIndex + 1) % statusOrder.length;
+    // return next status, or first one if its the last
+    return statusOrder[nextIndex];
+  };
+
+  // function that updates the order status
+  const updateOrderStatus = async (id: number, status: Status) => {
     try {
       const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/orders/${id}/status`, {
         method: "PATCH",
@@ -152,36 +195,53 @@ function AdminUser() {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ status }),
+        body: JSON.stringify({ newStatus: capitalizeFirstLetter(status) }),
       });
       const updatedOrder = await response.json();
+      console.log(status);
+      console.log(updatedOrder);
       // Assuming that the updatedOrder includes the updated order details
       // You may need to update the records state accordingly
+      // Refresh the user interface
+      await fetchData();
     } catch (error) {
       console.error("Error updating order status:", error);
     }
   };
 
+  // function that executes when the user click the button to update orderStatus
+  const handleButtonClick = async (userID: number, orderStatus: Status) => {
+    try {
+      console.log(orderStatus);
+      // activate the function that changes between status
+      const nextStatus = getNextStatus(orderStatus);
+      // call function that changes the order's status
+      await updateOrderStatus(userID, nextStatus as Status);
+    } catch (error) {
+      console.error('error handling the button', error);
+      return console.log(error);
+    }
+  };
+
   //!!!!!
-//   const handleRowClick = async (row: Order, _: MouseEvent<HTMLTableRowElement, MouseEvent>) => {
-//     console.log('Row clicked:', row);
-//     const newStatus: OrderStatus = { status: Status.Shipped }; // Update status as needed
-//     console.log(newStatus)
-//     try {
-//       await updateOrderStatus(row.id, newStatus);
-//       // Refresh data or update UI as necessary
-//     } catch (error) {
-//       console.error('Error updating order status:', error);
-//     }
-//   };
-  
-  
+  //   const handleRowClick = async (row: Order, _: MouseEvent<HTMLTableRowElement, MouseEvent>) => {
+  //     console.log('Row clicked:', row);
+  //     const newStatus: OrderStatus = { status: Status.Shipped }; // Update status as needed
+  //     console.log(newStatus)
+  //     try {
+  //       await updateOrderStatus(row.id, newStatus);
+  //       // Refresh data or update UI as necessary
+  //     } catch (error) {
+  //       console.error('Error updating order status:', error);
+  //     }
+  //   };
+
+
 
   return (
     <div
-      className={`bg-[#111827] h-screen ${
-        isTabletOrMobile ? "flex-col" : "flex"
-      }`}
+      className={`bg-[#111827] h-screen ${isTabletOrMobile ? "flex-col" : "flex"
+        }`}
     >
       {!isTabletOrMobile && <SideBar />}
       {isTabletOrMobile && <HamburguerMenuAdmin />}
@@ -210,7 +270,7 @@ function AdminUser() {
             fixedHeader
             progressPending={loading}
             selectableRows
-            // onRowClicked={handleRowClick as any}
+          // onRowClicked={handleRowClick as any}
           />
         </div>
       </div>
