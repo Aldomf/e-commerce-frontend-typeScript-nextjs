db9e0de2940eca9e91afb18975e996eef0dc274c
Aldo Miralles <alditomiralles3@gmail.com>
2024-05-05 17:26:12 -0600

    adding favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ export const metadata: Metadata = {
   title: "Shipshop",
   description:
     "Shipshop is your one-stop destination for all your shopping needs. Discover a wide range of products including electronics, fashion, home decor, and much more. With seamless navigation and secure checkout, shopping at Shipshop is convenient and enjoyable. Explore the latest trends, find great deals, and have your purchases delivered right to your doorstep. Start shopping today and experience the ultimate online shopping experience with Shipshop.",
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png']
+  }
 };
 
 export default function RootLayout({
