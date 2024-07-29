64a551b5529b2f2d17e4e4a8c4b6e32d96f0dfc9
Aldo Miralles <alditomiralles3@gmail.com>
2024-07-28 20:19:55 -0600

    fixing error 418

--- src/components/homapage/HeroLaptop.tsx
+++ src/components/homapage/HeroLaptop.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import ImageGallery from "react-image-gallery";
@@ -7,13 +8,11 @@ import "react-image-gallery/styles/css/image-gallery.css";
 interface CustomImageGalleryItem {
   original: string;
   description: JSX.Element;
-  //button: string;
 }
 
 interface ReactImageGalleryItem {
   original: string;
   description: string;
-  //button: string;
 }
 
 function HeroLaptop() {
@@ -31,7 +30,6 @@ function HeroLaptop() {
           <span className="text-4xl">Favorite Brands</span>
         </div>
       ),
-      //button: "Shop Now",
     },
     {
       original: "/back-5.jpg",
@@ -46,7 +44,6 @@ function HeroLaptop() {
           <span className="text-4xl">Favorite Brands</span>
         </div>
       ),
-      //button: "Shop Now",
     },
     {
       original: "/back-1.jpg",
@@ -61,7 +58,6 @@ function HeroLaptop() {
           <span className="text-4xl">Favorite Brands</span>
         </div>
       ),
-      //button: "Shop Now",
     },
     {
       original: "/back-55.jpg",
@@ -76,22 +72,18 @@ function HeroLaptop() {
           <span className="text-4xl">Favorite Brands</span>
         </div>
       ),
-      //button: "Shop Now",
     },
   ];
-
-  const convertedSlides: ReactImageGalleryItem[] = slides.map((slide) => ({
-    original: slide.original,
-    description: slide.description.toString(), // Convert JSX element to string
-    //button: slide.button,
-  }));
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <div className="relative mt-[214px] lg:mt-[228px] xl:mt-[244px]">
       <ImageGallery
-        items={convertedSlides}
+        items={slides.map(slide => ({
+          original: slide.original,
+          description: slide.description.props.children.join(" "), // Convert JSX to string
+        }))}
         showPlayButton={false}
         showFullscreenButton={false}
         showThumbnails={false}
@@ -99,23 +91,25 @@ function HeroLaptop() {
         autoPlay={true}
         slideInterval={10000}
         slideDuration={1000}
-        onSlide={(currentIndex) => setCurrentIndex(currentIndex)}
+        onSlide={(index) => setCurrentIndex(index)}
         renderItem={(item) => (
           <div className="relative md:h-[600px]">
             <div className="absolute top-0 left-0 w-full h-full bg-[#FEEED2] opacity-20"></div>
-            <img
+            <Image
               src={item.original}
-              alt={item.description}
+              alt={item.description || "No description available"}
+              width={1000}
+              height={1000}
               className="w-full h-full object-cover"
             />
             <div className="absolute top-20 left-20 lg:left-32 xl:top-28 xl:left-64 text-white flex flex-col items-start">
               <div className="mb-2 text-center md:mb-6">
                 {slides[currentIndex].description}
               </div>
-              {/* <button className="bg-[#A3C9BC] text-white px-10 py-2 rounded-full text-base transition duration-500 ease-in-out hover:text-[#A3C9BC] hover:border-[#A3C9BC] border-2 border-white hover:bg-white">
-                {slides[currentIndex].button}
-              </button> */}
-              <Link href="/category/all" className="bg-[#A3C9BC] text-white px-10 py-2 rounded-full text-base transition duration-500 ease-in-out hover:text-[#A3C9BC] hover:border-[#A3C9BC] border-2 border-white hover:bg-white">
+              <Link
+                href="/category/all"
+                className="bg-[#A3C9BC] text-white px-10 py-2 rounded-full text-base transition duration-500 ease-in-out hover:text-[#A3C9BC] hover:border-[#A3C9BC] border-2 border-white hover:bg-white"
+              >
                 Shop Now
               </Link>
             </div>
